e16f24c2bead2452840e8c6ab6f227ddaf784027
alessio <alessiochen98@gmail.com>
2021-11-11 16:43:11 +0100

    dynamic cells

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,27 +1,27 @@
 <template>
-  <div :board="board" class="play-area">
-    <div id="block_0" @click="handleClick(0)" class="block">{{ board[0] }}</div>
-    <div id="block_1" @click="handleClick(1)" class="block">{{ board[1] }}</div>
-    <div id="block_2" @click="handleClick(2)" class="block">{{ board[2] }}</div>
-    <div id="block_3" @click="handleClick(3)" class="block">{{ board[3] }}</div>
-    <div id="block_4" @click="handleClick(4)" class="block">{{ board[4] }}</div>
-    <div id="block_5" @click="handleClick(5)" class="block">{{ board[5] }}</div>
-    <div id="block_6" @click="handleClick(6)" class="block">{{ board[6] }}</div>
-    <div id="block_7" @click="handleClick(7)" class="block">{{ board[7] }}</div>
-    <div id="block_8" @click="handleClick(8)" class="block">{{ board[8] }}</div>
+  <div class="play-area">
+    <div
+      v-for="(item, index) in board"
+      :key="index"
+      :id="'block_' + index"
+      class="block"
+      @click="handleClick(index)"
+    >
+      {{ board[index] }}
+    </div>
   </div>
+
   <button @click="handleReset" class="reset-button">RESET BOARD</button>
   <h2 v-if="winner">{{ winner }}</h2>
 </template>
 
 <script>
-import { computed, reactive, ref } from "@vue/reactivity";
+import { computed, reactive, ref, toRefs } from "@vue/reactivity";
 export default {
   name: "GameBoard",
 
   setup() {
     let currentPlayer = "x";
-    let moves = 0;
     let board = reactive(["", "", "", "", "", "", "", "", ""]);
 
     const winner = computed(() => checkWin());
@@ -38,15 +38,19 @@ export default {
     ];
 
     const handleClick = (blockId) => {
-      if (winner.value) return;
-
-      board[blockId] = currentPlayer;
-      currentPlayer = currentPlayer === "o" ? "x" : "o";
+      if (winner.value) {
+        return;
+      }
+      if (board[blockId] == "") {
+        board[blockId] = currentPlayer;
+        currentPlayer = currentPlayer === "o" ? "x" : "o";
+      }
     };
 
     const handleReset = () => {
       for (let i = 0; i < board.length; i++) board[i] = "";
     };
+
     const checkWin = () => {
       for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
